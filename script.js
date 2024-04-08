e6379e66c26a56c9346e8f4c73a01a43fe013290
Zeynep <zeynepkargi02@gmail.com>
2024-04-09 00:11:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function p5() {
+  window.location.href = "p5.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+function p2() {
+  window.location.href = "p2.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+function p3() {
+  window.location.href = "p3.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+function p4() {
+  window.location.href = "p4.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+function p1() {
+  window.location.href = "p1.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+function p7() {
+  window.location.href = "p7.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+function p8() {
+  window.location.href = "p8.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+function p9() {
+  window.location.href = "p9.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+
+function p10() {
+  window.location.href = "p10.html"; // Geçmek istediğiniz sayfanın URL'sini buraya yazın
+}
+
+
